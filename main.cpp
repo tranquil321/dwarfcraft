c4a59f809d85209c601229e24f67c4a8e8226771
Steven Vandal <Steven@UNIT-00.local>
2014-06-02 17:13:34 -0400

    Added the world, controller, and graphics classes, and fixed a lot of compile-time errors. There are stil a few left, so I have yet to run the code.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
+#include <ncurses.h>
+#include <unistd.h>
+#include <stdio.h>
+
+class Graphics
+{
+public:
+	void initialize(){
+		initscr();
+		noecho();
+		curs_set(FALSE);
+	}
+	void draw(char* sprite, int x, int y){
+		mvprintw(x,y,sprite);
+	}
+	void refreshScreen(){
+		refresh();
+	}
+};
+
 class World
 {
 public:
+	World(){
+		WIDTH = 10;
+		HEIGHT = 10;
+		for (int i = 0; i < HEIGHT; i++){
+			for (int j = 0; j < WIDTH; j++){
+				if (i==0 || i==9 || j==0 || j==9){
+					worldArray[i][j] = "1";
+				} else {
+					worldArray[i][j] = "0";
+				}
+			}
+		}
+	}
+
 	//Returns true if the block is passable, otherwise returns false
 	bool resolveCollision(int x, int y){
 		if (worldArray[x][y] == 0){
@@ -8,22 +42,42 @@ public:
 		} 
 		else return false;
 	}
+	void update(Graphics& graphics){
+		for (int i = 0; i < HEIGHT; i++){
+			for (int j = 0; j < WIDTH; j++){
+				graphics.draw(worldArray[i][j],i,j);
+			}
+		}
+	}
 private:
-	int worldArray[10][10] = {{1,1,1,1,1,1,1,1,1,1},
-							  {1,0,0,0,0,0,0,0,0,1},
-							  {1,0,0,0,0,0,0,0,0,1},
-							  {1,0,0,1,1,0,0,0,0,1},
-							  {1,0,0,1,0,0,0,1,0,1},
-							  {1,1,0,0,0,0,0,1,0,1},
-							  {1,0,0,0,0,0,0,0,0,1},
-							  {1,0,0,0,1,1,0,0,0,1},
-							  {1,0,0,0,0,0,0,0,0,1},
-							  {1,1,1,1,1,1,1,1,1,1}};
-}
+	int WIDTH;
+	int HEIGHT;
+	char* worldArray[10][10];
+};
 
-class Graphics
+
 
 class Controller
+{
+public:
+	void initialize(){
+		nodelay(stdscr,TRUE);
+	}
+	bool kbhit(){
+		int ch = getch();
+		if (ch != ERR){
+			ungetch(ch);
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
+	static int getDirection(){
+		return getch();
+	}
+};
+
+class GameObject;
 
 class InputComponent
 {
@@ -52,7 +106,7 @@ public:
 	int x,y;
 	int xv,yv;
 	
-	Gameobject(InputComponent* input,
+	GameObject(InputComponent* input,
 			   PhysicsComponent* physics,
 			   GraphicsComponent* graphics)
 	: input_(input),
@@ -69,7 +123,7 @@ public:
 private:
 	InputComponent* input_;
 	PhysicsComponent* physics_;
-	graphicsComponent* graphics_;
+	GraphicsComponent* graphics_;
 };
 
 
@@ -79,24 +133,25 @@ class PlayerInputComponent : public InputComponent
 public:
 	virtual void update(GameObject& obj){
 		switch (Controller::getDirection()){
-			case W:
-				player.yVelocity = -1;
+			case KEY_UP:
+				obj.yv = -1;
 				break;
-			case S:
-				player.yVelocity += 1;
+			case KEY_DOWN:
+				obj.yv = 1;
 				break;
-			case A:
-				player.xVelocity = -1;
+			case KEY_LEFT:
+				obj.xv = -1;
 				break;
-			case D:
-				player.xVelocity = 1;
+			case KEY_RIGHT:
+				obj.xv = 1;
 				break;
 		}
 	}
-}
+};
 
 class MobPhysicsComponent : public PhysicsComponent
 {
+public:
 	virtual void update(GameObject& obj, World& world){
 		if (world.resolveCollision(obj.x + obj.xv, obj.y + obj.yv)){
 			obj.x += obj.xv;
@@ -105,25 +160,45 @@ class MobPhysicsComponent : public PhysicsComponent
 		obj.xv = 0;
 		obj.yv = 0;
 	}
-}
+};
 
 class MobGraphicsComponent : public GraphicsComponent
 {
 public:
+	MobGraphicsComponent(){
+		spriteNormal = "@";
+	}
 	virtual void update(GameObject& obj, Graphics& graphics){
 		char* sprite = spriteNormal;
 		//if mob is hurt, change, etc.
-		graphics.draw(sprite, player.x, player.y)
+		graphics.draw(sprite, obj.x, obj.y);
 	}
 private:
-	char* spriteNormal = '@';
+	char* spriteNormal;
 	//spriteHurt, spriteThirsty, etc.
 	//additionally add support for blinking sprites
-}
+};
 
 GameObject* createPlayer()
 {
-	return new GameObject(new PlayerInputComponent(),
-						  new MobPhysicsComponent(),
-						  new MobGraphicsComponent());
+	return new GameObject(new PlayerInputComponent(), new MobPhysicsComponent(), new MobGraphicsComponent());
+}
+
+int main(int argc, char* argv[]){
+	Graphics* graphics = new Graphics();
+	graphics->initialize();
+	
+	World* world = new World();
+	
+	Controller* controller = new Controller();
+	controller->initialize();
+	
+	GameObject* Player = createPlayer();
+	
+	while (true){
+		Player->update(world, graphics);
+		world->update(graphics);
+		refresh();
+	}
+		
 }
